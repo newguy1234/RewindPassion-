fc996f1e08593cc4659243a334f359e2a5b8f59e
psytb12 <psytb12@nottingham.ac.uk>
2025-06-05 16:13:54 +0100

    First screen designed, new pages added

--- RewindPassion/app/src/main/java/com/example/rewindpassion/HostActivity.java
+++ RewindPassion/app/src/main/java/com/example/rewindpassion/HostActivity.java
@@ -1,8 +1,6 @@
 package com.example.rewindpassion;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,32 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class HostActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.activity_host);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });
-
-        Button joinButton = findViewById(R.id.joinButton);
-        Button hostButton = findViewById(R.id.hostButton);
-
-        // Button listeners
-        joinButton.setOnClickListener(v -> {
-            Intent intent = new Intent(MainActivity.this, JoinActivity.class);
-            startActivity(intent);
-        });
-
-        hostButton.setOnClickListener(v -> {
-            Intent intent = new Intent(MainActivity.this, HostActivity.class);
-            startActivity(intent);
         });
     }
 }
